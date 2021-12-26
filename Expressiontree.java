723e4caae9e3e5905dbb750d4ef4f88341a1e180
j2jang2024dolphin <96479806+j2jang2024dolphin@users.noreply.github.com>
2021-12-26 16:53:17 +0900

    Create Expressiontree.java

---
+++ Expressiontree.java
@@ -0,0 +1,64 @@
+/*
+    ExpressionTree.java
+*/
+
+import DS.BinaryTree;
+
+public class ExpressionTree {
+    // get closing bracket's index
+    // given an expression and index of opening bracket
+    public static int closingIndex(String exp, int openIndex) {
+        int count = 1; // bracket counter; open: +1, close: -1
+        int index = openIndex + 1;
+        for (; index < exp.length(); index++) {
+            char ch = exp.charAt(index);
+            if (ch == '(')
+                count++;
+            else if (ch == ')')
+                count--;
+
+            // if count is 0, it is closing index
+            if (count == 0)
+                break;
+        }
+        return index;
+    }
+
+    // construct a tree with String expression
+    // only with + - * / ( ) and a number
+    // assumption:
+    // 1. only two operand if no bracket. e.g. 1+2+3 will be given as (1+2)+3
+    // 2. number is one-digit positive integer
+    public static BinaryTree construct(String exp) {
+        System.out.println(exp);
+        BinaryTree root = new BinaryTree();
+
+        /* left operand */
+        int opIndex = 1; // index of operator
+        if (Character.isDigit(exp.charAt(0))) {
+            root.setLeft(exp.charAt(0));
+        } 
+       else {
+            int index = closingIndex(exp, 0);
+            BinaryTree left = construct(exp.substring(0, closingIndex(exp, 0));
+            left.parent = root;
+            root.left = left;
+
+            opIndex = index + 1;
+        }
+
+        /* operator */
+        root.elem = Character.toString(exp.charAt(opIndex));
+
+        /* right operand */
+        if (Character.isDigit(exp.charAt(exp.length() - 1))) {
+            root.setRight(exp.substring(opIndex + 1, opIndex + 2));
+        } else {
+            BinaryTree right = construct(exp.substring(exp.charAt(opIndex + 1), exp.length() - 1));
+            right.parent = root;
+            root.right = right;
+        }
+
+        return root;
+    }
+}
