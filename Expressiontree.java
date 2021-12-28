e040ef6df6d063aa1f2834cd4a8c2df874ef7949
Junhyeok Jang <96479806+j2jang2024dolphin@users.noreply.github.com>
2021-12-28 20:12:56 +0900

    Update Expressiontree.java

--- Expressiontree.java
+++ Expressiontree.java
@@ -2,8 +2,6 @@
     ExpressionTree.java
 */
 
-import DS.BinaryTree;
-
 public class ExpressionTree {
     // get closing bracket's index
     // given an expression and index of opening bracket
@@ -30,17 +28,15 @@ public class ExpressionTree {
     // 1. only two operand if no bracket. e.g. 1+2+3 will be given as (1+2)+3
     // 2. number is one-digit positive integer
     public static BinaryTree construct(String exp) {
-        System.out.println(exp);
         BinaryTree root = new BinaryTree();
 
         /* left operand */
         int opIndex = 1; // index of operator
         if (Character.isDigit(exp.charAt(0))) {
-            root.setLeft(exp.charAt(0));
-        } 
-       else {
+            root.setLeft(exp.substring(0, 1));
+        } else {
             int index = closingIndex(exp, 0);
-            BinaryTree left = construct(exp.substring(0, closingIndex(exp, 0));
+            BinaryTree left = construct(exp.substring(1, index));
             left.parent = root;
             root.left = left;
 
@@ -51,14 +47,71 @@ public class ExpressionTree {
         root.elem = Character.toString(exp.charAt(opIndex));
 
         /* right operand */
-        if (Character.isDigit(exp.charAt(exp.length() - 1))) {
+        if (Character.isDigit(exp.charAt(opIndex + 1))) {
             root.setRight(exp.substring(opIndex + 1, opIndex + 2));
         } else {
-            BinaryTree right = construct(exp.substring(exp.charAt(opIndex + 1), exp.length() - 1));
+            BinaryTree right = construct(exp.substring(opIndex + 2, exp.length() - 1));
             right.parent = root;
             root.right = right;
         }
 
         return root;
+    }
+    
+    public static double checkDigit(String k) {
+    	String[] digits = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    	for (String i : digits) {
+    		if (k.equals(i)) {
+    			return Double.parseDouble(i);
+    		}
+    	}
+    	return -1.0;
+    }
+
+    // calculate the constructed tree
+    public static double calculate(BinaryTree tree) {
+    	double leftside;
+    	double rightside;
+    	if (checkDigit(tree.left.elem) != -1.0) {
+    		leftside = checkDigit(tree.left.elem);
+    		System.out.println("X");
+    	}
+    	else {
+    		leftside = calculate(tree.left);
+    	}
+    	if (checkDigit(tree.right.elem) != -1.0) {
+    		rightside = checkDigit(tree.right.elem);
+    		System.out.println("X");
+    	}
+    	else {
+        	rightside = calculate(tree.right);
+    	}
+    	System.out.println(leftside);
+    	System.out.println(tree.elem);
+    	System.out.println(rightside);
+        if (tree.elem.equals(String.valueOf('+'))) {
+        	double out = leftside + rightside;
+        	return out;
+        }
+        else if (tree.elem.equals(String.valueOf('-'))) {
+        	double out = leftside - rightside;
+        	return out;
+        }
+        else if (tree.elem.equals(String.valueOf('*'))) {
+        	double out = leftside * rightside;
+        	return out;
+        }
+        else if (tree.elem.equals(String.valueOf('/'))) {
+        	double out = leftside / rightside;
+        	return out;
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = construct("1+3");
+        //System.out.println(tree.left.elem);
+        double result = calculate(tree);
+
+        System.out.println(result);
     }
 }
